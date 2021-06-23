9ee571181aeef17fcf4fcfcd791cf8ed2ece2b6f
antwan ola <antwanola29@gmail.com>
2021-06-23 06:35:59 +0100

    Update dev-db.js

--- config/dev-db.js
+++ config/dev-db.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    mongoDbUrl: 'mongodb://localhost/Portfolio'
+    mongoDbUrl: 'mongodb://antwan:comodor29@cluster0-shard-00-00.84jxs.mongodb.net:27017,cluster0-shard-00-01.84jxs.mongodb.net:27017,cluster0-shard-00-02.84jxs.mongodb.net:27017/test?replicaSet=atlas-jlsxxg-shard-0&ssl=true&authSource=admin'
 
 };
